68f23fbc165aa8766b8c02b96de31e14e5f909c7
jean-paulJm <jpjm_95@hotmail.fr>
2017-06-09 17:16:52 +0200

    Delete lrs_properties.py

--- lrs_properties.py
+++
@@ -1,9 +0,0 @@
-"""
-Contains user-specific information for testing.
-"""
-
-
-endpoint="http://cloud.scorm.com/tc/6OICOM4D18/sandbox/"
-version ="1.0.1" # 1.0.1 | 1.0.0 | 0.95 | 0.9
-username="68e3XfwpMYf9pu_0aAQ"
-password="Y8DaYubfO7VNzBAbU2k"
